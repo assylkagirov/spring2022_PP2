1611d69997a42b3a4be15f410a612a40556a15c9
Assylzhan Kagirov <98634686+assylkagirov@users.noreply.github.com>
2022-04-15 20:03:01 +0600

    Updated

--- lab8/3.py
+++ lab8/3.py
@@ -1,25 +1,24 @@
 import pygame
-
-pygame.display.set_caption("Line - l, circle - c, rect - t, eracer - e. Red - r, green - g, blue - b")
-
+pygame.display.set_caption("Current mode: Brush. Current color: Blue." + "; c=circle; p=rectangle; l=brush; r=red; g=green; b=blue")
+data = []
+elements = 0
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
     
     radius = 15
-    x = 0
-    y = 0
-    mode = 'blue'
-    points = []
-    drawmode = 0
+    mode = 'Blue'
+    drawmode = 'Brush'
+    oldmode = 'Brush'
+    oldcmode = 'Blue'
     while True:
         
         pressed = pygame.key.get_pressed()
         
         alt_held = pressed[pygame.K_LALT] or pressed[pygame.K_RALT]
         ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
-        
+
         for event in pygame.event.get():
             
             # determin if X was clicked, or Ctrl+W or Alt+F4 was used
@@ -35,71 +34,65 @@ def main():
             
                 # determine if a letter key was pressed
                 if event.key == pygame.K_r:
-                    mode = 'red'
+                    mode = 'Red'
                 elif event.key == pygame.K_g:
-                    mode = 'green'
+                    mode = 'Green'
                 elif event.key == pygame.K_b:
-                    mode = 'blue'
-                
-                if event.key == pygame.K_c:
-                    drawmode = 1
-                elif event.key == pygame.K_t:
-                    drawmode = 2
-                elif event.key == pygame.K_e:
-                    drawmode = 3
-                elif event.key == pygame.K_l:
-                    drawmode = 0
-
+                    mode = 'Blue'
+                #checking shapes
+                if event.key == pygame.K_l:
+                    drawmode = 'Brush'
+                elif event.key == pygame.K_c:
+                    drawmode = 'Circle'
+                elif event.key == pygame.K_p:
+                    drawmode = 'Rectangle'
+                if oldmode != drawmode:
+                    oldmode = drawmode
+                    pygame.display.set_caption("Current mode: "+drawmode+" Current color: "+mode+ "; c=circle; p=rectangle; l=brush; r=red; g=green; b=blue") #update paint status
+                if oldcmode != mode:
+                    oldcmode = mode
+                    pygame.display.set_caption("Current mode: "+drawmode+" Current color: "+mode+ "; c=circle; p=rectangle; l=brush; r=red; g=green; b=blue") #update paint status
+            screen.fill((0, 0, 0))    
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1: # left click grows radius
+                if event.button == 1: #check if clicked then add to draw
+                    drawadd(event.pos, radius, mode, drawmode)
+                if event.button == 4: #check mousewhell to incr/decr the radius
                     radius = min(200, radius + 1)
-                elif event.button == 3: # right click shrinks radius
+                elif event.button == 5:
                     radius = max(1, radius - 1)
-            
-            if event.type == pygame.MOUSEMOTION:
-                # if mouse moved, add point to list
-                position = event.pos
-                points = points + [position]
-                points = points[-256:]
                 
-        screen.fill((0, 0, 0))
-        
-        # draw all points
-        i = 0
-        while i < len(points) - 1:
-            if drawmode == 3:
-                points.clear()
-            else: 
-                drawLineBetween(screen, i, points[i], points[i + 1], radius, mode, drawmode)
-            i += 1
-        
+        draw(screen)
         pygame.display.flip()
         
         clock.tick(60)
 
-def drawLineBetween(screen, index, start, end, width, color_mode, draw_mode):
-    c1 = max(0, min(255, 2 * index - 256))
-    c2 = max(0, min(255, 2 * index))
-    
-    if color_mode == 'blue':
-        color = (c1, c1, c2)
-    elif color_mode == 'red':
-        color = (c2, c1, c1)
-    elif color_mode == 'green':
-        color = (c1, c2, c1)
-    
-    dx = start[0] - end[0]
-    dy = start[1] - end[1]
-    iterations = max(abs(dx), abs(dy))
-    for i in range(iterations):
-        progress = 1.0 * i / iterations
-        aprogress = 1 - progress
-        x = int(aprogress * start[0] + progress * end[0])
-        y = int(aprogress * start[1] + progress * end[1])
-        if draw_mode == 0:
-            pygame.draw.line(screen, color, (x, y), (x + width, y+width))
-        elif draw_mode == 1:
-            pygame.draw.circle(screen, color, (x, y), width, 3)
-        elif draw_mode == 2:
-            pygame.draw.rect(screen, color, pygame.Rect(x, y, x + width, y + width))
+def drawadd(start, width, color_mode, draw_mode): #add to draw function
+    global data, elements
+    if color_mode == 'Blue': #color kmode
+        color = (0, 0, 255)
+    elif color_mode == 'Red':
+        color = (255, 0, 0)
+    elif color_mode == 'Green':
+        color = (0, 255, 0)
+    clist = [] #creating data list for our drawing
+    clist.append(start[0])
+    clist.append(start[1])
+    clist.append(width)
+    clist.append(color)
+    clist.append(draw_mode)
+    data.append(clist) #appending this list to gen data
+    elements += 1
+
+def draw(screen):
+    global data, elements #getting global vars
+    if elements != 0: #checking if elements exist
+        for element in data:
+            if element[4] == 'Brush': #checking every possible shape
+                pygame.draw.circle(screen, element[3], (element[0], element[1]), element[2])
+            elif element[4] == 'Circle':
+                pygame.draw.circle(screen, element[3], (element[0], element[1]), element[2], 1)
+            elif element[4] == 'Rectangle':
+                pygame.draw.rect(screen, element[3], pygame.Rect(element[0], element[1], element[2], element[2] / 2), 1)
+            elif element[4] == 'Eraser':
+                pygame.draw.circle(screen, (0,0,0), (element[0], element[1]), element[2])
 main()
